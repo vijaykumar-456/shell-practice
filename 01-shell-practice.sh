b64f609c9f2da85d5d336b56a5161fec00dbe408
Vk167 <viijaykumarr07@gmail.com>
2026-05-22 21:12:31 +0530

    conversation

--- 01-shell-practice.sh
+++ 01-shell-practice.sh
@@ -2,3 +2,8 @@
 echo "Hello-World"
 echo "my second attempt"
 echo "my third attempt"
+
+echo "Trump: Hey Iran, I am going to blast you"
+echo "Iran: Oh nice"
+echo "Trump: I am serious Iran"
+echo "Iran: I am also serious"
