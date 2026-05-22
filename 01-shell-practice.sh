507b070df9b8cad762e8db3260beb65627caee96
Vk167 <viijaykumarr07@gmail.com>
2026-05-22 21:20:49 +0530

    conversaation with dollar symbol

--- 01-shell-practice.sh
+++ 01-shell-practice.sh
@@ -3,8 +3,8 @@ echo "Hello-World"
 echo "my second attempt"
 echo "my third attempt"
 
-PERSON1=India
-PERSON2=Pakistan
+PERSON1=$1
+PERSON2=$2
 echo "$PERSON1: Hey $PERSON2, I am going to blast you"
 echo "$PERSON2: Oh nice"
 echo "$PERSON1: I am serious $PERSON2"
