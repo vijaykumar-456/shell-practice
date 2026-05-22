1020aa5b52a757f3015848caccbe9f8f8cdf8553
Vk167 <viijaykumarr07@gmail.com>
2026-05-22 21:18:00 +0530

    conversation

--- 01-shell-practice.sh
+++ 01-shell-practice.sh
@@ -3,8 +3,8 @@ echo "Hello-World"
 echo "my second attempt"
 echo "my third attempt"
 
-PERSON1=Trump
-PERSON2=Iran
+PERSON1=India
+PERSON2=Pakistan
 echo "$PERSON1: Hey $PERSON2, I am going to blast you"
 echo "$PERSON2: Oh nice"
 echo "$PERSON1: I am serious $PERSON2"
