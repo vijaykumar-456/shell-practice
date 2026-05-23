5943b743aa93000a018c43b51867c8bba0a9de8d
Vk167 <viijaykumarr07@gmail.com>
2026-05-23 11:39:24 +0530

    read variable

--- 02-special-variable.sh
+++ 02-special-variable.sh
@@ -1,3 +1,11 @@
 #!bin/bash
-echo "hello Hi"
-echo "this is my second shell scripting file"
+
+echo "Please enter your name:"
+read USER_NAME #here username is the variable, whatever you enter in the terminal stores in the variable
+
+echo "your username : $USERNAME"
+
+echo "Please enter your password:"
+read PASSWORD
+
+echo "your password : $PASSWORD"
