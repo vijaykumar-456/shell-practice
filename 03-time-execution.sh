3fbae08eddd45a1fcc9b39e8c7ca5a55b7c285c5
Vk167 <viijaykumarr07@gmail.com>
2026-05-23 11:59:07 +0530

    time-stamp

--- 03-time-execution.sh
+++ 03-time-execution.sh
@@ -6,7 +6,14 @@ echo " Current Time is : $TIMESTAMP"
 
 # calculate start and end time
 
-START_TIME=$(date)
-END_TIME=$(date)
+START_TIME=$(date +%s)
+
+sleep 10
+
+END_TIME=$(date +%s)
 
 echo "execution time is: $START_TIME - $END_TIME"
+
+TOTAL_TIME = $(($START_TIME - $END_TIME))
+
+echo " Script executed in: $TOTAL_TIME seconds"
