818bea4ae31216b42e6f8ddbf1a579115991d612
Vk167 <viijaykumarr07@gmail.com>
2026-05-23 11:45:14 +0530

    read variable

--- 02-special-variable.sh
+++ 02-special-variable.sh
@@ -3,9 +3,21 @@
 echo "Please enter your name:"
 read USER_NAME #here username is the variable, whatever you enter in the terminal stores in the variable
 
-echo "your username : $USERNAME"
+echo "your name is $USER_NAME"
 
 echo "Please enter your password:"
 read PASSWORD
 
 echo "your password : $PASSWORD"
+
+# Hiding the username and password while typing / entering
+
+echo "please enter your name:"
+read -s USER_NAME
+
+echo "This is your name: $USER_NAME"
+
+echo "please enter your Password:"
+read -s PASSWORD
+
+echo "This is your password: $PASSWORD"
