ee222d3af5ee8960b09081723ee8ebb97d84d7ca
Vk167 <viijaykumarr07@gmail.com>
2026-05-23 11:56:56 +0530

    time-stamp

--- 03-time-execution.sh
+++ 03-time-execution.sh
@@ -3,3 +3,10 @@
 TIMESTAMP=$(date)
 
 echo " Current Time is : $TIMESTAMP"
+
+# calculate start and end time
+
+START_TIME=$(date)
+END_TIME=$(date)
+
+echo "execution time is: $START_TIME - $END_TIME"
