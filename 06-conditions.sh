8cc5d556365869edfa21ba980dbb43b84db4c9d8
Vk167 <viijaykumarr07@gmail.com>
2026-05-23 19:05:18 +0530

    conditions

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -3,4 +3,8 @@
 NUMBER=$1
 if [ $NUMBER -ge 20 ]; then
     echo "Given number $NUMBER is greater than 20"
+elif [ $NUMBER -gt 10]; then
+    echo "Given number $NUMBER is greater than 10"
+else
+    echo "Given number $NUMBER is less than 10"
 fi
