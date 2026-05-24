394d10b8f9d609590f17b256f762b16c8923550f
Vk167 <viijaykumarr07@gmail.com>
2026-05-24 19:19:54 +0530

    automate process

--- 08-automate-install.sh
+++ 08-automate-install.sh
@@ -9,3 +9,17 @@ if [ $USERID -ne 0 ]; then
 fi
 
 echo "I am continuing..."
+
+dnf list installed mysql
+
+if [ $? -eq 0 ]; then
+    echo " Already mysql installed ... SKIP"
+else
+    dnf install mysql -y
+    if [ $? -ne 0]; then
+        echo " Installation Failed .... Failed"
+        exit 1
+    else
+        echo " Mysql installed ... Success"
+    fi
+fi
