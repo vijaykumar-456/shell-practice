2d0cb5845233eba974bdcbaa88450ec409ee9b2f
Vk167 <viijaykumarr07@gmail.com>
2026-06-04 08:07:16 +0530

    new commit

--- 10-loops.sh
+++ 10-loops.sh
@@ -1,6 +1,28 @@
 !#bin/bash
-for num in {1..10}:
+#!bin/bash
+LOG_DIR=/var/log/shell-script
+LOG_FILES="$LOG_DIR/$0.log"
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo " Try with Sudo access"
+    exit 1
+
+fi
+
+VALIDATE(){
+     if [ $2 -ne 0 ]; then
+        echo " Installation $1 .... Failed" | tee -a $LOG_FILES
+        exit 1
+    else
+        echo " $1 installed ... Success" | tee -a $LOG_FILES
+    fi
+}
+
+for package in $@
 do 
-    echo $num
+    echo $package
 done
+
 
