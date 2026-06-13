90d25b1e739730a5cef7b7827f10204ef2c95548
Vk167 <viijaykumarr07@gmail.com>
2026-06-13 09:14:07 +0530

    delete log files

--- 14-loops-cleanup.sh
+++ 14-loops-cleanup.sh
@@ -13,3 +13,18 @@ if [ ! -d $SOURCE_DIR ]; then
     echo "ERROR: Directory:$SOURCE_DIR not exist"
     exit 1
 fi
+
+echo "Scanning $SOURCE_DIR for log files older than 14 days"
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
+
+if [ -z $FILES ]; then
+    echo "No log files older than $DAYS found"
+    echo 0
+fi
+
+while IFS= read -r FILE
+do
+    echo "File to be deleted: $FILE"
+    rm -f $FILE
+    echo "File $FILE is deleted"
+done <<< "$FILES"
