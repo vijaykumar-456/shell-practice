576049e1e3bb779598c2c3d093a8da432f2a8e64
Vk167 <viijaykumarr07@gmail.com>
2026-06-13 16:01:36 +0530

    disk usage

--- 17-mail.sh
+++ 17-mail.sh
@@ -8,7 +8,7 @@ TO_ADDRESS=$5
 SUBJECT=$6
 
 FINAL_MESSAGE=$( echo $MESSAGE | sed -e 's/[&/]/\\&/g' )
-FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$SERVER_IP/g", -e "S/MESSAGE/$FINAL_MESSAGE/g" template.html)
+FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$SERVER_IP/g", -e "s/MESSAGE/$FINAL_MESSAGE/g" template.html)
 
 {
 echo "To: $TO_ADDRESS"
