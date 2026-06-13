35d9b705495a9578c5b9f4fef9ffcf1e12a7354e
Vk167 <viijaykumarr07@gmail.com>
2026-06-13 15:07:16 +0530

    disk usage

--- 16-dis-usage.sh
+++ 16-dis-usage.sh
@@ -7,7 +7,7 @@ while IFS= read -r  line
 do 
     USAGE=$( echo $line | awk '{print $6}' | cut -d "%" -f1 )
     PARTITION=$( echo $line | awk '{ print $7 }' )
-    if [ "$USAGE" -ge "$USAGE_THRESHOLD"]; then
+    if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
         MESSAGE="High Disk usage on $PARTITION: $USAGE"
     fi
 done <<< "$DISK_USAGE"
