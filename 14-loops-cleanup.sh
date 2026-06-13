419cc452e1270091c069eb447c230281b656a0e8
Vk167 <viijaykumarr07@gmail.com>
2026-06-13 09:24:43 +0530

    delete log files

--- 14-loops-cleanup.sh
+++ 14-loops-cleanup.sh
@@ -8,13 +8,13 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-if [ -z $SOURCE_DIR ]; then
+if [ -z "$SOURCE_DIR" ]; then
     echo "ERROR: missing parameter"
     echo "USAGE: $0 <SOURCE_DIR> [Days[Optional]]"
     exit 1
 fi
 
-if [ ! -d $SOURCE_DIR ]; then
+if [ ! -d "$SOURCE_DIR" ]; then
     echo "ERROR: Directory:$SOURCE_DIR not exist"
     exit 1
 fi
